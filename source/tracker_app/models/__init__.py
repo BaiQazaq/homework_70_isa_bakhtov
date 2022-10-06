6d6db60d69c0e53db7711e8596cfaca97b4c7448
Isa Bakhtov <issab79@icloud.com>
2022-10-07 01:04:22 +0600

    1.Удален файл views.py 2.Созданы папки templates, views и пустой файл urls.py в приложении tracker_app 3.Из модели Task в файле model_task.py из поля type удален свойство on_delete=models.PROTECT

--- source/tracker_app/models/__init__.py
+++ source/tracker_app/models/__init__.py
@@ -1,3 +1,3 @@
-from model_task import Task
-from model_type import Type
-from model_status import Status
+from .model_task import Task
+from .model_type import Type
+from .model_status import Status
