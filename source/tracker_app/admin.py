5e804e2b59767d9a92805862adefa67f5d525c0f
Isa Bakhtov <issab79@icloud.com>
2022-10-07 00:44:06 +0600

    В папке admin.py зарегистрирован модель или создан class TaskAdmin

--- source/tracker_app/admin.py
+++ source/tracker_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from tracker_app.models import Task
+
 # Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("id","summary", "description", "status", "type", "created_at")
+    list_filter = ("id", "summary", "description", "status", "type", "created_at")
+    search_fields = ("summary", "description", "status", "type")
+    fields = ("summary", "description", "status", "type", "created_at", "changed_at")
+    readonly_fields = ("id", "created_at", "changed_at")
+
+admin.site.register(Task, TaskAdmin)
