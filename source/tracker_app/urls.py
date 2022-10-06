335287dadd17e5b07d194d49503e8af939d419f6
Isa Bakhtov <issab79@icloud.com>
2022-10-07 01:36:08 +0600

    Связали вьюху и страницу в файле tracker_app/urls.py с именем index

--- source/tracker_app/urls.py
+++ source/tracker_app/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
-from tracker_app.views.base import base_view
-from tracker_app.views.tasks_methods import add_view, detail_view, update_view, delete_view, confirm_delete
+from tracker_app.views.tasks_view import IndexView
+#from tracker_app.views.tasks_methods import add_view, detail_view, update_view, delete_view, confirm_delete
 
 urlpatterns = [
-    path("", base_view, name='base'),
-    path("tasks/add/", add_view, name='creation'),
-    path("task/<int:pk>/update/", update_view, name='update'),
-    path("task/<int:pk>", detail_view, name='show'),
-    path("task/<int:pk>/delete/", delete_view, name= "delete"),
-    path("task/<int:pk>/confirm_delete/", confirm_delete, name= "confirm_delete")
+    path("", IndexView.as_view(), name='index')
+    # path("tasks/add/", add_view, name='creation'),
+    # path("task/<int:pk>/update/", update_view, name='update'),
+    # path("task/<int:pk>", detail_view, name='show'),
+    # path("task/<int:pk>/delete/", delete_view, name= "delete"),
+    # path("task/<int:pk>/confirm_delete/", confirm_delete, name= "confirm_delete")
 ]
