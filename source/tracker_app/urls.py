4eae0142d5d07f7c7e8cdaed67c9e2d6fe691828
Isa Bakhtov <issab79@icloud.com>
2022-10-07 19:01:44 +0600

    Создана страница по удалению задачи и вьюха к ней и пути зарегистрованы в urls.py. Страница запускается, удаление работает

--- source/tracker_app/urls.py
+++ source/tracker_app/urls.py
@@ -4,6 +4,7 @@ from tracker_app.views.tasks_view import IndexView
 from tracker_app.views.task_view import TaskView
 from tracker_app.views.task_edit_view import TaskEditView
 from tracker_app.views.task_create_view import TaskCreateView
+from tracker_app.views.task_delete import delete_view, confirm_delete
 
 
 urlpatterns = [
@@ -11,6 +12,6 @@ urlpatterns = [
     path("task/<int:pk>", TaskView.as_view(), name='task_render'),
     path("task/<int:pk>/update/", TaskEditView.as_view(), name='task_edit'),
     path("tasks/add/", TaskCreateView.as_view(), name='task_creation'), 
-    # path("task/<int:pk>/delete/", delete_view, name= "delete"),
-    # path("task/<int:pk>/confirm_delete/", confirm_delete, name= "confirm_delete")
+    path("task/<int:pk>/delete/", delete_view, name= "delete"),
+    path("task/<int:pk>/confirm_delete/", confirm_delete, name= "confirm_delete")
 ]
---
+++ source/tracker_app/views/task_delete.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from tracker_app.models import Task
+
+
+def delete_view(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
+    return render(request, 'task_confirm_delete.html', context={'task': task})
+
+
+def confirm_delete(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
+    return redirect('base')
