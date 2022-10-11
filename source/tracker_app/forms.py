53fd85d7c28078a45b22aff1861361c94b8cd551
Isa Bakhtov <issab79@icloud.com>
2022-10-11 18:48:19 +0600

    Произвели дамп

--- source/tracker_app/forms.py
+++ source/tracker_app/forms.py
@@ -1,12 +1,17 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from django.core.validators import MaxLengthValidator, MinLengthValidator
 
 from tracker_app.models import Task
-from tracker_app.models import Type
-from tracker_app.models import Status
 
+def upper_validator(string):
+    if not string[0].isupper():
+        raise ValidationError('First letter must be in Uppercase')
+    return string
+    
 
 class TaskForm(forms.ModelForm):
+    summary = forms.CharField(label='Заголовок', max_length=100, required=True, validators=(MaxLengthValidator(limit_value=100), MinLengthValidator(limit_value=2), upper_validator))
         
     class Meta:
         model = Task
@@ -14,8 +19,10 @@ class TaskForm(forms.ModelForm):
     
     def clean_summary(self):
         summary = self.cleaned_data.get('summary')
-        if len(summary) < 2:
-            raise ValidationError('Заголовок должен быть длинее 2x символов')
+        # if len(summary) < 2:
+        #     raise ValidationError('This value is too short!')
+        if Task.objects.filter(summary=summary).exists():
+            raise ValidationError ("Text of the article should not duplicate it's title!")
         return summary
     
     
--- source/tracker_app/views/task_create_view.py
+++ source/tracker_app/views/task_create_view.py
@@ -1,32 +1,7 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from tracker_app.models import Task, Status, Type
-from django.views.generic import TemplateView, FormView
+from django.views.generic import TemplateView
 from tracker_app.forms import TaskForm
-from django.urls import reverse
-
-
-# class TaskCreateView(FormView):
-#     template_name = 'task_create.html'
-#     extra_context = {
-#         'statuses': Status.objects.all(),
-#         'types' : Type.objects.all()
-#         }
-
-
-
-#     def get_form_kwargs(self):
-#         kwargs = super().get_form_kwargs()
-#         #kwargs['instance'] = self.task
-#         return kwargs
-
-
-#     def form_valid(self, form):
-#         self.task = form.save()
-#         return super().form_valid(form)
-
-
-#     def get_success_url(self):
-#         return reverse('task_render', kwargs={'pk': self.task.pk})
 
 
 
@@ -51,4 +26,4 @@ class TaskCreateView(TemplateView):
             )
             task.type.set(type)
             return redirect('task_render', pk=task.pk)
-        return render(request, 'task_creation.html', context={'form': form})
+        return render(request, 'task_create.html', context={'form': form})
