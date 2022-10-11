d590dd080e17706260833bbe062fd7921d67efe9
Isa Bakhtov <issab79@icloud.com>
2022-10-11 15:55:24 +0600

    Корректировки незначительные формы

--- source/tracker_app/views/task_edit_view.py
+++ source/tracker_app/views/task_edit_view.py
@@ -1,28 +1,63 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
 from tracker_app.forms import TaskForm
 from tracker_app.models import Task
 
 
-class TaskEditView(TemplateView):
+class TaskEditView(FormView):
     template_name = 'task_edit.html'
-    
+    form_class = TaskForm
+
+
+    def dispatch(self, request, *args, **kwargs):
+        self.task = self.get_object()
+        return super().dispatch(request, *args, **kwargs)
+
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['task'] = get_object_or_404(Task, pk=kwargs['pk'])
+        context['task'] = self.task
         return context
+
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['instance'] = self.task
+        return kwargs
+
+
+    def form_valid(self, form):
+        self.task = form.save()
+        return super().form_valid(form)
+
+
+    def get_success_url(self):
+        return reverse('task_render', kwargs={'pk': self.task.pk})
+
+
+    def get_object(self):
+        pk = self.kwargs.get('pk')
+        return get_object_or_404(Task, pk=pk)
+
+# class TaskEditView(TemplateView):
+#     template_name = 'task_edit.html'
     
-    def get(self, request, *args, **kwargs):
-        context = self.get_context_data(**kwargs)
-        form = TaskForm(instance=context['task'])
-        context['form'] = form
-        return self.render_to_response(context)
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['task'] = get_object_or_404(Task, pk=kwargs['pk'])
+#         return context
     
-    def post(self, request, *args, **kwargs):
-        task = get_object_or_404(Task, pk=kwargs['pk'])
-        form = TaskForm(request.POST, instance=task)
-        if form.is_valid():
-            task = form.save()
-            return redirect('task_render', pk=task.pk)
-        return render(request, 'task_edit.html', context={'form': form})
+#     def get(self, request, *args, **kwargs):
+#         context = self.get_context_data(**kwargs)
+#         form = TaskForm(instance=context['task'])
+#         context['form'] = form
+#         return self.render_to_response(context)
+    
+#     def post(self, request, *args, **kwargs):
+#         task = get_object_or_404(Task, pk=kwargs['pk'])
+#         form = TaskForm(request.POST, instance=task)
+#         if form.is_valid():
+#             task = form.save()
+#             return redirect('task_render', pk=task.pk)
+#         return render(request, 'task_edit.html', context={'form': form})
         
