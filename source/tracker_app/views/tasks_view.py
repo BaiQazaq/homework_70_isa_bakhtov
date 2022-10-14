fd2da438b2874f8cf255f0d4498b2e0b14ec721c
Isa Bakhtov <issab79@icloud.com>
2022-10-14 16:08:22 +0600

    1. Применили ListView для представления обхектов прописали вьюху 2.Предусмотрена пагинация по 10 задача во вьюхе 3.Представление pagination.html создано 4.Добавлены стили пагниации 5.В шаблон index.html добавлен блок отображения пагинации 6.На странице отобрадения деталей задачи добавлена ссылка на созадение задачи

--- source/tracker_app/views/tasks_view.py
+++ source/tracker_app/views/tasks_view.py
@@ -1,11 +1,10 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
 from tracker_app.models import Task
 
 
-class IndexView(TemplateView):
+class IndexView(ListView):
     template_name = 'index.html'
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['tasks'] = Task.objects.filter(is_deleted=False)
-        return context
+    model = Task
+    context_object_name = 'tasks'
+    ordering = ('-created_at',)
+    paginate_by = 10
